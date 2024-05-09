2aca4c562b8357b5e15e8e2452a69ded293a23fd
jos8304 <jyou8304@gmail.com>
2024-05-09 19:59:20 -0400

    Add comment editing and deleting functions

--- podomarket/urls.py
+++ podomarket/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
 
     #comment
     path('posts/<int:post_id>/comments/create', views.CommentCreateView.as_view(), name='comment-create'),
+    path('comments/<int:comment_id>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comments/<int:comment_id>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
