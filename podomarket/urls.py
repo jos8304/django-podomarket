e45e6c0d315ca288295d5860245056ad27c51412
jos8304 <jyou8304@gmail.com>
2024-05-09 19:43:19 -0400

    Create mixins.py

--- podomarket/urls.py
+++ podomarket/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     path('users/<int:user_id>/posts/', views.UserPostListView.as_view(), name='user-post-list'),
     path('set-profile/', views.ProfileSetView.as_view(), name='profile-set'),
     path('edit-profile/', views.ProfileUpdateView.as_view(), name='profile-update'),
+
+    #comment
+    path('posts/<int:post_id>/comments/create', views.CommentCreateView.as_view(), name='comment-create'),
 ]
--- podomarket/views.py
+++ podomarket/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from allauth.account.models import EmailAddress
 from allauth.account.views import PasswordChangeView
 
-from braces.views import LoginRequiredMixin, UserPassesTestMixin
+from braces.views import LoginRequiredMixin
+from .mixins import LoginAndVerificationRequiredMixin, LoginAndOwnershipRequiredMixin
 
 from .forms import PostCreateForm, PostUpdateForm, ProfileForm,CommentForm
-from .functions import confirmation_required_redirect
-from .models import Post, User
+from .models import Post, User, Comment
 
 
 class IndexView(ListView):
@@ -33,13 +32,11 @@ class PostDetailView(DetailView):
         return context
         
 
-class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+class PostCreateView(LoginAndVerificationRequiredMixin, CreateView):
     model = Post
     form_class = PostCreateForm
     template_name = 'podomarket/post_form.html'
 
-    redirect_unauthenticated_users = True
-    raise_exception = confirmation_required_redirect
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -48,41 +45,39 @@ class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     def get_success_url(self):
         return reverse('post-detail', kwargs={'post_id': self.object.id})
 
-    def test_func(self, user):
-        return EmailAddress.objects.filter(user=user, verified=True).exists()
 
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class PostUpdateView(LoginAndOwnershipRequiredMixin, UpdateView):
     model = Post
     form_class = PostUpdateForm
     template_name = 'podomarket/post_form.html'
     pk_url_kwarg = 'post_id'
 
-    redirect_unauthenticated_users = False
-    raise_exception = True
 
     def get_success_url(self):
         return reverse('post-detail', kwargs={'post_id': self.object.id})
 
-    def test_func(self, user):
-        post = self.get_object()
-        return post.author == user
 
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+class PostDeleteView(LoginAndOwnershipRequiredMixin, DeleteView):
     model = Post
     template_name = 'podomarket/post_confirm_delete.html'
     pk_url_kwarg = 'post_id'
     
-    redirect_unauthenticated_users = False
-    raise_exception = True
-
     def get_success_url(self):
         return reverse('index')
 
-    def test_func(self, user):
-        post = self.get_object()
-        return post.author == user
+class CommentCreateView(LoginAndVerificationRequiredMixin, CreateView):
+    http_method_names = ['post']
+
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = Post.objects.get(id=self.kwargs.get('post_id'))        
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'post_id': self.kwargs.get('post_id')})
 
 
 class ProfileView(DetailView):
